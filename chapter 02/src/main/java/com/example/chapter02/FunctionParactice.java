4e992e532ba0841599b7a2813a1fe2bbe60973e4
anas <syedanasalihashmi57@gmail.com>
2022-01-05 14:49:36 +0500

    paractice of java 1-by making a table

---
+++ chapter 02/src/main/java/com/example/chapter02/FunctionParactice.java
@@ -0,0 +1,14 @@
+package com.example.chapter02;
+
+import java.util.ArrayList;
+
+public class FunctionParactice {
+   public static void main(String args[]){
+       int a=6;
+       int j;
+       for(int i=0;i<10;i++){
+           j=i+1;
+           System.out.println(a + "x"+ j + "=" + a*j);
+       }
+   }
+}
---
+++ chapter 02/src/main/java/com/example/chapter02/MyClass.java
@@ -0,0 +1,7 @@
+package com.example.chapter02;
+
+public class MyClass {
+    public static void main(String args[]){
+        System.out.println("Hello world");
+    }
+}
